25514bcadbaa52018c265e12089de922eec2259d
soukarya <soukaryadatta@gmail.com>
2022-05-08 21:10:14 +0530

    Initial version

---
+++ DataStructureAssignmentSolution/src/com/greatllearning/dsa/assignment/q1/factory/Factory.java
@@ -0,0 +1,46 @@
+package com.greatllearning.dsa.assignment.q1.factory;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.greatllearning.dsa.assignment.q1.singleton.Singleton;
+import com.greatllearning.dsa.assignment.q1.skyscraper.Skyscraper;
+
+public class Factory {
+
+	private List<Integer> floorListFromfactory = null;
+	
+	public List<Integer> getFloorsFromFactory() {
+		return this.floorListFromfactory;
+	}
+	
+	public static void input(final int totalFloorsInTheBuilding) {
+		
+		final Scanner sc = Singleton.getScannerInstance();
+		Factory factory = Singleton.getFactoryInstance();
+		int floorSize = 0;
+		Skyscraper skyscraper = null;
+		
+		factory.floorListFromfactory = new ArrayList<Integer>(totalFloorsInTheBuilding);
+		skyscraper = new Skyscraper(totalFloorsInTheBuilding);
+		
+		// factory is producing the floors that will be given to architect by factories on the ith day  
+		for(int i=0;i<skyscraper.getTotalFloorsInTheBuilding();) {
+			System.out.println("enter the floor size given on day : "+(i+1));
+			floorSize = sc.nextInt();
+			if(factory.getFloorsFromFactory().contains(floorSize)) {
+				System.err.println("duplicate value exist!!! Please enter distinct values between 1 and "+skyscraper.getTotalFloorsInTheBuilding()+"\n");
+			} else if(floorSize>skyscraper.getTotalFloorsInTheBuilding()){
+				System.err.println("Invalid!!! Entered number cannot be greater than "+skyscraper.getTotalFloorsInTheBuilding()+"\n");
+			} else if(floorSize<=0){
+				System.err.println("Invalid!!! Entered number cannot be less than or equal to 0"+"\n");
+			}else {
+				factory.getFloorsFromFactory().add(floorSize);
+				i++;
+			}
+		}
+		skyscraper = null;
+		sc.close();
+	}	
+}
---
+++ DataStructureAssignmentSolution/src/com/greatllearning/dsa/assignment/q2/main/Main.java
@@ -0,0 +1,86 @@
+package com.greatllearning.dsa.assignment.q2.main;
+
+import java.util.Scanner;
+
+import com.greatllearning.dsa.assignment.q2.bst.BstCreation;
+import com.greatllearning.dsa.assignment.q2.bst.BstCreation.Node;
+
+public class Main {
+	
+	static Node prevNode = null;
+	static Node headNode = null;
+
+	static void toRightSkewed(Node root) {
+
+		if (root == null) {
+			return;
+		}
+		
+		Main.toRightSkewed(root.getLeft()/*, order*/);
+		
+		Node rightNode = root.getRight();
+
+		
+		if (headNode == null) {
+			headNode = root;
+			root.setLeft(null);
+			prevNode = root;
+		} else {
+			prevNode.setRight(root);
+			root.setLeft(null);
+			prevNode = root;
+		}
+		
+
+		Main.toRightSkewed(rightNode/*, order*/);
+	}
+
+	static void traverseRightSkewed(Node root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.getKey() + " ");
+		traverseRightSkewed(root.getRight());
+	}
+
+
+	public static void main(String[] args) {
+		//       50
+		//      /  \
+		//     30  60
+		//     /   /
+		//    10  55
+		
+		int num=-1;
+		int value=0;
+		Node root = null;
+		Node result = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter total no of values: ");
+		num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			System.out.print("Insert value at " + (i + 1) + ": ");
+			value = sc.nextInt();
+			if (root == null) {
+				root = BstCreation.insert(root, value);
+			} else {
+				result = BstCreation.insert(root, value);
+				if (result.getKey() == value)
+					i -= 1;
+			}	
+		}
+		
+		System.out.println("\nInorder traversal of this BST");
+		BstCreation.inOrderTraversal(root);
+
+		toRightSkewed(root);
+		
+		// output
+		System.out.println("\n\nBinary search tree after converting to right skewed BST");
+		traverseRightSkewed(headNode);
+		System.out.println("\n\n***** Program completed successfully *****");
+		
+		root = null;
+		sc.close();
+	}
+}
